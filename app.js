223b387499dfab9b3cbf735e5818ace9f521db9e
Maddie <maddiedeming1@gmail.com>
2018-01-28 10:49:26 -0500

    playing around with auth

--- app.js
+++ app.js
@@ -22,54 +22,53 @@ function toggleSignIn(){
     else{
         firebase.auth().signOut();
     }
-    document.getElementById('quickstart-sign-in').disabled = true;
+    $("#login").disabled = true;
 }
 
 function initApp(){
     firebase.auth().getRedirectResult().then(function(result){
     if (result.credential){
         var token = result.credential.accessToken;
-        document.getElementById('quickstart-oauthtoken').textContent = token;
+       //$("#quickstart-oauthtoken").textContent = token;
     } 
     else{
-        document.getElementById('quickstart-oauthtoken').textContent = 'null';
+        //$("#quickstart-oauthtoken").textContent = null;
     }
     var user = result.user;
     }).catch(function(error){
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    if (errorCode === 'auth/account-exists-with-different-credential') {
-        alert('You have already signed up with a different auth provider for that email.');
-    } 
-    else {
-        console.error(error);
-    }
-});
-
-    firebase.auth().onAuthStateChanged(function(user){
-      if (user){
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-        document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-        document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-      } 
-      else{
-        document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-        document.getElementById('quickstart-sign-in').textContent = 'Sign in with GitHub';
-        document.getElementById('quickstart-account-details').textContent = 'null';
-        document.getElementById('quickstart-oauthtoken').textContent = 'null';
-      }
-    document.getElementById('quickstart-sign-in').disabled = false;
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        if(errorCode === "auth/account-exists-with-different-credential") {
+            alert("You have already signed up with a different auth provider for that email.");
+        } 
+        else {
+            console.error(error);
+        }
     });
-    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+    firebase.auth().onAuthStateChanged(function(user){
+        if (user){
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            //$("#quickstart-sign-in-status").textContent = "Signed in";
+            //$("#login").textContent = "Sign out";
+            //$("#quickstart-account-details").textContent = JSON.stringify(user, null, "  ");
+        } 
+        else{
+            //$("#quickstart-sign-in-status").textContent = "Signed out";
+            //$("#login").textContent = "Sign in with GitHub";
+            //$("#quickstart-account-details").textContent = null;
+            //$("#quickstart-oauthtoken").textContent = null;
+        }
+        $("#login").disabled = false;
+    });
+    $("#login").addEventListener('click', toggleSignIn, false);
 }
 window.onload = function(){
     initApp();
