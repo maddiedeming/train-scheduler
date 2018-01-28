1202dd20e1cdad3999409a44fd2afdcaf7c935bf
Maddie <maddiedeming1@gmail.com>
2018-01-28 10:25:50 -0500

    auth again

--- app.js
+++ app.js
@@ -12,19 +12,68 @@ var config = {
   };
 firebase.initializeApp(config);
 var database = firebase.database();
-var provider = new firebase.auth.GithubAuthProvider();
-firebase.auth().signInWithRedirect(provider);
-firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-      var token = result.credential.accessToken;
+
+function toggleSignIn(){
+    if (!firebase.auth().currentUser){
+        var provider = new firebase.auth.GithubAuthProvider();
+        provider.addScope('repo');
+        firebase.auth().signInWithRedirect(provider);
+    } 
+    else{
+        firebase.auth().signOut();
+    }
+    document.getElementById('quickstart-sign-in').disabled = true;
+}
+
+function initApp(){
+    firebase.auth().getRedirectResult().then(function(result){
+    if (result.credential){
+        var token = result.credential.accessToken;
+        document.getElementById('quickstart-oauthtoken').textContent = token;
+    } 
+    else{
+        document.getElementById('quickstart-oauthtoken').textContent = 'null';
     }
     var user = result.user;
-  }).catch(function(error) {
+    }).catch(function(error){
     var errorCode = error.code;
     var errorMessage = error.message;
     var email = error.email;
     var credential = error.credential;
-  });
+    if (errorCode === 'auth/account-exists-with-different-credential') {
+        alert('You have already signed up with a different auth provider for that email.');
+    } 
+    else {
+        console.error(error);
+    }
+});
+
+    firebase.auth().onAuthStateChanged(function(user){
+      if (user){
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+        document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+        document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+      } 
+      else{
+        document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+        document.getElementById('quickstart-sign-in').textContent = 'Sign in with GitHub';
+        document.getElementById('quickstart-account-details').textContent = 'null';
+        document.getElementById('quickstart-oauthtoken').textContent = 'null';
+      }
+    document.getElementById('quickstart-sign-in').disabled = false;
+    });
+    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+}
+window.onload = function(){
+    initApp();
+};
 
 function calculateTimes(frequency,minutes){
     _firstTrain = moment().startOf('day').minute(minutes); 
