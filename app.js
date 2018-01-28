fee3453b91b29adc0b877dfba6dfefd259c1d74b
Maddie <maddiedeming1@gmail.com>
2018-01-27 19:20:27 -0500

    Requirements completed.

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-//Global Variables
-var trainName = $("#trainName");
-var destination = $("#destination");
-var firstTrainTime = $("#firstTrainTime");
-var frequency = $("#frequency");
-var add = $("#add");
+
+var _minutesAway;
+var _nextArrival = "";
+
+$("#trainTable").empty();
 //Firebase
 var config = {
     apiKey: "AIzaSyDnAUlx9qlnBAf17mXq0NmW9UFvwFnJqV0",
@@ -14,45 +13,55 @@ var config = {
     messagingSenderId: "372011317998"
 };
 firebase.initializeApp(config);
+var database = firebase.database();
 
-$(frequency).blur(function(){
-    var _inputFrequency = parseInt(frequency.val().trim());
-    if(_inputFrequency > 60){
-        frequency.val(60);
+function calculateTimes(frequency,minutes){
+    _firstTrain = moment().startOf('day').minute(minutes); 
+    var number = moment().diff(moment(_firstTrain), "minutes");
+    
+    if(number < 0){
+        number = number * -1;    
+        var currentTime = parseInt(((moment().get('hour')) * 60) + (moment().get('minutes')));
+        var trainTime = parseInt(((_firstTrain.get('hour')) * 60) + (_firstTrain.get('minutes')));
+        _minutesAway = trainTime - currentTime
+        _nextArrival = _firstTrain.format("h:mm a");
     }
-});
-
-$(firstTrainTime).blur(function(){
-    if(firstTrainTime[0].type != "time"){
-        var _firstTrainTime = firstTrainTime.val().trim();
-        //Fix Shit For Internet Explorer and Safari
+    else{
+        _minutesAway = parseInt(frequency - (number % frequency));
+        _nextArrival = moment().add(_minutesAway, 'minutes').format("h:mm a");
     }
-});
+}
 
-$(add).on("click", function(event){
+$("#add").on("click", function(event){
     event.preventDefault();
-    var _inputTrainName = trainName.val().trim();
-    var _inputDestination = destination.val().trim();
-    var _inputFirstTrainTime = firstTrainTime.val().trim();
-    var _inputFrequency = frequency.val().trim();
-    //Clear Input Values
-    if(_inputTrainName != null){
-        trainName.val("");
+    var _trainName = $("#trainName").val().trim();
+    var _destination = $("#destination").val().trim();
+    var _firstTrain = $("#firstTrainTime").val().trim();
+    var _firstTrainMinutes = parseInt((_firstTrain.charAt(0) + _firstTrain.charAt(1)) * 60) + parseInt(_firstTrain.charAt(3) + _firstTrain.charAt(4));
+    var _frequency = $("#frequency").val().trim();
+    $("input").val("");
+    calculateTimes(_frequency,_firstTrainMinutes);
+    var train = {
+        trainName: _trainName,
+        destination: _destination,
+        frequency: _frequency,
+        nextArrival: _nextArrival,
+        minutesAway: _minutesAway       
     }
-    if(_inputDestination != null){
-        destination.val("");
-    }
-    if(_inputFirstTrainTime != null){
-        firstTrainTime.val("");
-    }
-    if(_inputFrequency != null){
-        frequency.val("");
-    }
+    database.ref().push(train);
 });
 
-//Integrate DOM Manipulation Logic When You Learn How To Use Firebase
-var trainTable = $("#trainTable");
-var tableRow = $("<tr>");
-var tableColumn = $("<td><td><td><td><td>");
-tableRow.append(tableColumn);
-trainTable.append(tableRow);
+database.ref().on("child_added", function(snapshot){
+    var trainTable = $("#trainTable");
+    var tableRow = $("<tr>");
+    var tableColumn = 
+        $('<td>' + snapshot.val().trainName + '</td>' +
+        '<td>' + snapshot.val().destination + '</td>' +
+        '<td>' + snapshot.val().frequency + '</td>' +
+        '<td>' + snapshot.val().nextArrival + '</td>' +
+        '<td>' + snapshot.val().minutesAway + '</td>');
+    tableRow.append(tableColumn);
+    trainTable.append(tableRow);
+  }, function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+  });
