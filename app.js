efa9e3d4ffdfe50dcc2fd5efcdeaca3397f7c15e
Maddie <maddiedeming1@gmail.com>
2018-01-28 09:22:07 -0500

    auth

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
+var provider = new firebase.auth.GithubAuthProvider();
+firebase.auth().signInWithRedirect(provider);
+
+firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+      var token = result.credential.accessToken;
+    }
+    var user = result.user;
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+  });
+
 function calculateTimes(frequency,minutes){
     _firstTrain = moment().startOf('day').minute(minutes); 
     var number = moment().diff(moment(_firstTrain), "minutes");
